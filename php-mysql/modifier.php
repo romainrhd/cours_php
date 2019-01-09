e7ffd45c40bc9d92174ba016f8390ac69389ee7b
RomZor <romain.richard.19@gmail.com>
2019-01-09 16:12:17 +0100

    :tada: Ajout de la modification

---
+++ php-mysql/modifier.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'includes/head.php';
+require_once 'includes/connexion.php';
+
+if (!empty($_POST)) {
+    $query = $pdo->prepare('UPDATE personnes
+                                      SET name = :name, firstname = :firstname
+                                      WHERE id = :id');
+    $query->execute([
+        'id' => $_POST['id'],
+        'name' => $_POST['name'],
+        'firstname' => $_POST['firstname'],
+    ]);
+    header('Location: index.php');
+} else {
+    $query = $pdo->prepare('SELECT * FROM personnes WHERE id = :id');
+    $query->execute([
+        'id' => $_GET['id']
+    ]);
+    $personne = $query->fetch(PDO::FETCH_ASSOC);
+}
+?>
+
+<form action="modifier.php" method="POST">
+    <label for="">Nom</label> <input type="text" name="name" value="<?= $personne['name'] ?>"><br>
+    <label for="">Prénom</label> <input type="text" name="firstname" value="<?= $personne['firstname'] ?>"><br>
+    <input type="hidden" name="id" value="<?= $personne['id'] ?>">
+    <input type="submit" value="Modifier">
+</form>
+
+<?php
+require_once 'includes/footer.php';
+?>
